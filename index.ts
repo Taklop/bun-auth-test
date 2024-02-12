0fa21e1312940463cf9737641475e590af0e08a1
Darien <daz.opperman@gmail.com>
2024-02-12 02:15:10 +0000

    add and use basic lib and tsconfig

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
+import figlet from 'figlet';
+
 const server = Bun.serve({
-    port: 3000,
-    fetch(req) {
-        return new Response("Bun!")
-    }
-})
+  port: 3000,
+  fetch(req) {
+    const body = figlet.textSync('Bun!');
+    return new Response(body);
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
+console.log(Bun.version);
