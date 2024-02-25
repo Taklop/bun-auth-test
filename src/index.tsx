da6e396c4f354b1814491694677291120378af1e
Darien <daz.opperman@gmail.com>
2024-02-25 14:00:45 +0000

    Add prisma and make dummy user creation input

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
 import { html } from '@elysiajs/html';
 import * as elements from 'typed-html';
 import { logger } from '@bogeychan/elysia-logger';
+import { PrismaClient } from '@prisma/client';
 
 import BaseHtml from './BaseHtml';
 
 const app = new Elysia();
+const prisma = new PrismaClient();
 app.use(html());
 app.use(logger({ level: 'debug' }));
 
@@ -29,11 +31,50 @@ app.get('/', ({ html }: any) =>
       >
         Click me
       </button>
+
+      <form hx-post="/api/user" hx-swap="outerHTML" class="flex flex-col gap-2">
+        <label for="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          class="border rounded-lg solid border-white/35 p-2 bg-white/15 hover:bg-white/25 focus:bg-white/25 active:bg-white/35 transition-colors duration-200 ease-in-out"
+        />
+        <button
+          type="submit"
+          class="border rounded-lg solid border-white/35 p-2 bg-white/15 hover:bg-white/25 focus:bg-white/25 active:bg-white/35 transition-colors duration-200 ease-in-out"
+        >
+          Submit
+        </button>
+      </form>
     </BaseHtml>
   )
 );
 
 app.post('/clicked', () => <div>I was clicked!</div>);
+
+app.post(
+  '/api/user',
+  async ({ body }) => {
+    if (body.name.length === 0) {
+      throw new Error('Content is empty');
+    }
+    try {
+      await prisma.user.create({
+        data: {
+          name: body.name,
+        },
+      });
+    } catch (error) {
+      throw new Error('Error creating user');
+    }
+  },
+  {
+    body: t.Object({
+      name: t.String(),
+    }),
+  }
+);
 
 app.listen(3000);
 
